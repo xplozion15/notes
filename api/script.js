eedba5f050e2002cf9e7594c2867318e822b7ad1
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-12-21 17:02:33 +0530

    chore: define prisma models

---
+++ api/script.js
@@ -0,0 +1,3 @@
+import { prisma } from '../api/lib/prisma.js';
+
+console.log("from script js of api");
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Notes - A personal blog app!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, repellat
+        optio, exercitationem vel rerum assumenda cupiditate possimus quia
+        similique rem esse! Aperiam doloremque doloribus asperiores magni
+        veritatis repellendus praesentium qui soluta ut neque, accusantium quo
+        expedita sequi numquam, fuga molestiae earum ipsam nisi minima
+        laudantium repudiandae tempore omnis! Aliquam fugit, quod ab sit earum
+        dolorem consectetur, nemo aut ad mollitia, temporibus fuga. Fugit nisi,
+        facilis laborum sint, eius mollitia molestias pariatur ipsam ipsa totam
+        voluptatibus officia sit suscipit, deleniti obcaecati aliquam repellat.
+        Ipsa, est facilis quis quia doloribus adipisci deserunt laboriosam!
+        Expedita aperiam placeat, iusto quisquam nesciunt labore. Iste, cum quis
+        tempora dolorem maxime at totam, iure molestiae adipisci inventore nisi
+        doloribus fuga exercitationem dicta beatae natus id, modi libero
+        necessitatibus nihil. Culpa, eligendi iste officiis aut maxime repellat!
+        Non autem aut consequuntur impedit accusantium dolores id animi natus
+        obcaecati blanditiis voluptas minima nulla aliquid, pariatur laborum,
+        asperiores excepturi a cum? Molestias, harum fuga. Natus hic dolores ea
+        alias accusantium, temporibus harum aliquid officiis saepe, possimus
+        repudiandae perspiciatis qui vero ullam laudantium ex perferendis
+        asperiores sequi nesciunt, itaque eius! Deserunt excepturi non amet, nam
+        natus exercitationem placeat laudantium nisi, quaerat deleniti assumenda
+        quae aut earum doloribus? Nesciunt aliquam consectetur libero,
+        reiciendis omnis, nam magni dolores deleniti perferendis officiis nobis
+        quae quaerat veritatis facere neque, ad voluptates iure culpa qui. Nisi
+        aperiam repellat ipsa eveniet totam, vero maxime aspernatur quis aliquid
+        voluptatem. Beatae eum odio unde laborum officia consequatur quidem eius
+        reprehenderit incidunt officiis ipsum assumenda fugiat voluptatum illum,
+        voluptatem iure dolor ea itaque voluptate nisi doloremque distinctio
+        natus. Cum eos mollitia laborum libero explicabo voluptatem dicta enim
+        aperiam natus reprehenderit aliquid iusto illum, veritatis at dolor
+        fugit assumenda excepturi maiores cupiditate consequatur qui suscipit
+        reiciendis commodi! Id voluptate cum tempore ipsum, tempora maxime
+        voluptatem magnam deleniti soluta ducimus! Impedit quasi cumque dolorem
+        totam veniam magnam libero reiciendis dolorum hic, saepe, et deserunt
+        nam quidem sunt recusandae vitae similique pariatur, itaque aut. Tenetur
+        reiciendis expedita eveniet veniam error quasi dolores minus incidunt
+        doloremque, perspiciatis iste. Fugiat, quisquam asperiores. Similique
+        fugiat illo soluta error sequi tempore ex? Laborum a labore nulla cumque
+        corporis odio aspernatur aliquam, adipisci magnam repudiandae tempore,
+        similique consequatur quis doloribus eum vero distinctio ex? Optio
+        corrupti aliquid inventore reprehenderit necessitatibus, saepe officia
+        amet magnam, architecto in ut suscipit incidunt eligendi laboriosam
+        expedita similique totam explicabo nam iste vitae quo fugit! Repudiandae
+        sint nam tempora, tenetur eaque praesentium ea iusto omnis hic est earum
+        sapiente! Quas quos excepturi id fugiat adipisci obcaecati aperiam
+        doloribus voluptates, officiis ipsam veniam sunt quaerat aut sit facilis
+        animi sapiente sed consectetur sequi magni distinctio tempora
+        recusandae, dolorum ad! Tenetur, quidem dignissimos? Accusantium ratione
+        dolor unde dicta error fugiat voluptate doloribus repellat maiores eos
+        praesentium, voluptas totam aut illo perspiciatis temporibus reiciendis
+        cum recusandae odio quas adipisci tempora magnam architecto. Dolor, eum,
+        ab quo excepturi cum eaque in nam vitae, tempore quaerat neque corporis?
+        Sint earum quasi quos molestiae eius vero? Dolore, voluptates. Ratione
+        incidunt vitae ducimus nemo veniam maiores esse ipsam soluta
+        consectetur.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
