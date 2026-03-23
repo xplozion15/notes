8d1e236f4152172a2a7667536f252769910290b5
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2026-03-23 11:51:49 +0530

    feat: add backend validation for the edit category form in admin frontend

---
+++ api/validators/validateEditCategory.js
@@ -0,0 +1,13 @@
+const { body } = require("express-validator");
+
+const validateEditCategory = [
+  body("categoryId").isInt().withMessage("Category id should be a number"),
+  body("categoryTitle")
+    .trim()
+    .isEmpty()
+    .withMessage("Category title should not be empty")
+    .isLength({ min: 1, max: 15 })
+    .withMessage("Category title length should be 1 to 15 characters"),
+];
+
+export { validateEditCategory };
