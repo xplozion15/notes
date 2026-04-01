94a5d74ca3c9f4bd1fc6a70887271f99e595ab5e
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2026-04-01 19:35:45 +0530

    feat: add 404 route and both frontend's env variable in CORS line in app.js

--- api/app.js
+++ api/app.js
@@ -1,8 +1,8 @@
+//imports
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT;
-
+const port = process.env.PORT || 5000;
 const { postRouter } = require("./routes/post.routes");
 const { categoryRouter } = require("./routes/category.routes");
 const { commentRouter } = require("./routes/comment.routes");
@@ -10,11 +10,21 @@ const { authRouter } = require("./routes/auth.routes");
 const { statsRouter } = require("./routes/stats.routes");
 const cors = require("cors");
 
-//for form data to be accessed in the controllers
-app.use(cors({ origin: "http://localhost:5173" }));
+// Read the two frontend urls from env
+const allowedOrigins = [process.env.FRONTEND_URL_1, process.env.FRONTEND_URL_2];
+
+// Enable CORS for allowed frontends
+app.use(
+  cors({
+    origin: allowedOrigins,
+  }),
+);
+
+// for form handling in controller
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Routes
 app.use("/posts", postRouter);
 app.use("/categories", categoryRouter);
 app.use("/posts/:postId/comments", commentRouter);
@@ -22,6 +32,12 @@ app.use("/comments", commentRouter);
 app.use("/auth", authRouter);
 app.use("/stats", statsRouter);
 
+// error 404 route
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+//listener
 app.listen(port, () => {
   console.log(`Notes - a personal blog app ${port}`);
 });
